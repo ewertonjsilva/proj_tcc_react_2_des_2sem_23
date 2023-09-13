144b76c921521dde330f9c88616ca3553308ebe2
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-09-12 22:43:08 -0300

    aula 2 conversao html css
    usar biblioteca de ícones

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Header from '../../header/header';
 import Rodape from '../../footer/footer';
 
 function Home() {
   return (
     <div>
+      <Header />
       Home do site
       <Rodape />
     </div>
