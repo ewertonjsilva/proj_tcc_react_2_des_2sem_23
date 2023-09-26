61716d3ae9e90f83b1dc8a15286e930560575060
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-09-26 13:52:50 -0300

    header e menu finalizado
    Voltar a mexer após o login para mudar o texto login/logout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { MdFastfood, MdMenu } from 'react-icons/md';
 import { Link, useNavigate } from 'react-router-dom';
@@ -6,6 +6,17 @@ import { Link, useNavigate } from 'react-router-dom';
 import './header.css';
 
 function Cabecalho({ pag }) {
+
+  const [mobile, setMobile] = useState(false);
+
+  function ativaMenu() {
+    if (mobile === false) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  }
+
   return (
     <header>
       <nav className="containerNav">
@@ -22,10 +33,17 @@ function Cabecalho({ pag }) {
             <Link to='/login' className={pag === 'login' ? 'active' : ''}>Login</Link>
           </div>
           <div className="menuMobile">
-            <a href="#">
-              <MdMenu className="icon"/>
+            <a href="#" onClick={ativaMenu}>
+              <MdMenu className="icon" />
             </a>
           </div>
+        </div>
+        <div className={mobile === false ? "menuMobileExpandidon" : "menuMobileExpandidos"}>
+          <Link to='/' className={pag === 'home' ? 'active' : ''}>Home</Link>
+          <Link to='/listprod' className={pag === 'produtos' ? 'active' : ''}>Produtos</Link>
+          <Link to='/cadusu' className={pag === 'cadUsu' ? 'active' : ''}>Cadastrar</Link>
+          <Link to='/contato' className={pag === 'contato' ? 'active' : ''}>Contato</Link>
+          <Link to='/login' className={pag === 'login' ? 'active' : ''}>Login</Link>
         </div>
       </nav>
     </header>
