431951881ccac3b1c4f628989d1ff5791e3a2864
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-10-04 11:46:18 -0300

    material 9

--- src/components/pages/produtos/produto.jsx
+++ src/components/pages/produtos/produto.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import Cabecalho from '../../header/header';
@@ -11,21 +11,33 @@ import './produto.css'
 function Produto() {
 
   const location = useLocation();
+  /* 
+    Usar state para receber produto, pois ao diminiur o tamanho da tela, 
+    ao acionar o menu, a renderização de tela é necessária e com isso o produto
+    armazenado pela navegação é perdido, gerando erro
+  */
+ const [itemCarregado, setItemCarregado] = useState({});
+
+  useEffect(() => {
+    setItemCarregado(location.state);
+  }, []);
 
   return (
     <>
       <Cabecalho />
 
       <div className="container">
-        <img
-          className='imgProd'
-          src={location.state.prd_img}
-          alt={"Imagem " + location.state.prd_nome}
-        />
-        <div>
+        <div className='containerItem'>
+          <img
+            className='imagemProd'
+            src={location.state.prd_img}
+            alt={"Imagem " + location.state.prd_nome}
+          />
+        </div>
+        <div className='containerItem'>
           <div className="titulo">
-            <span id="titulo">{location.state.prd_nome}</span>
-            <img src={location.state.img_tp_prod} className="icon" alt={location.state.img_tp_prod}/>
+            <span id="titulo">{itemCarregado.prd_nome}</span>
+            <img src={location.state.img_tp_prod} className="icon" alt={location.state.img_tp_prod} />
           </div>
           <span className="descricao">{location.state.prd_descricao}</span>
           <span id="valor">{'R$ ' + location.state.prd_valor}</span>
